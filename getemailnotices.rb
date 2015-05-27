9ceab42fa5ead2bde0de830faff91eaf4991a1d8
dojobo <manguero@gmail.com>
2015-05-27 10:39:11 -0500

    improve filelist with loop

--- getemailnotices.rb
+++ getemailnotices.rb
@@ -12,30 +12,19 @@ Net::FTP.open(server) do |ftp|
 	ftp.login(user, pass)
 	print "connected to #{server} - "
 	puts ftp.pwd
-	begin
-		#filelist12 = ftp.nlst('em12*') # kludge to get around ftp server's limits; add to these as necessary
-		#filelist13 = ftp.nlst('em13*')
-		#filelist14 = ftp.nlst('em14*')
-		#filelist15 = ftp.nlst('em15*')
-		#filelist16 = ftp.nlst('em16*')
-		#filelist17 = ftp.nlst('em17*')
-		#filelist18 = ftp.nlst('em18*')
-		#filelist19 = ftp.nlst('em19*')
-		filelist20 = ftp.nlst('em20*')
-		filelist21 = ftp.nlst('em21*')
-	rescue Net::FTPPermError
-		puts "Filename scope larger than existing files."
-		filelist21 = []
-	end
-	#allfiles = filelist12 + filelist13 + filelist14 + filelist15 + filelist16 + filelist17 + filelist18 + filelist19 + filelist20 + filelist21
-	allfiles = filelist20 + filelist21
+	filelist = %w(em20* em21*).each_with_object([]) { |range, filelist|
+  	begin
+    	filelist.concat ftp.nlst(range)
+  	rescue Net::FTPPermError
+  	end
+	}
 	ARGV << Date.today.to_s if ARGV.empty?		# if no argument is given on command line, default to today's date
 	ARGV.each { |a|		# for each command-line arg, i.e. date...
 		FileUtils.mkdir_p("notices/#{a}")
 		checkdate = Date.parse(a)
 		puts checkdate
 		count = 0
-		allfiles.each { |file|
+		filelist.each { |file|
 			if ftp.mtime(file).to_date == checkdate then	# compare file's mtime to supplied check date
 				puts "getting #{file}..."
 				localpath = "notices/#{a}/#{file}"
