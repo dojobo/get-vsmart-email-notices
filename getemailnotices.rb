c9fb2dbf2c12d94555b33339e7249a5e158cafe2
dojobo <dominicbordelon@gmail.com>
2015-07-21 15:12:07 -0500

    wrap retries in rescue in case they all fail

--- getemailnotices.rb
+++ getemailnotices.rb
@@ -15,31 +15,37 @@ Net::FTP.open(server) do |ftp|
 	print "connected to #{server} - "
 	puts ftp.pwd
 	#filelist = %w(em10* em11* em12* em13* em14* em15* em16* em17* em18* em19* em20* em21* em22* em23*).each_with_object([]) { |range, filelist|
-	filelist = %w(em20* em21* em22* em23*).each_with_object([]) { |range, filelist|
+	filelist = %w(em20* em21* em22* em23*).each_with_object([]) do |range, filelist|
   	begin
     	filelist.concat ftp.nlst(range)
   	rescue Net::FTPPermError
   	end
-	}
+	end
 	ARGV << Date.today.to_s if ARGV.empty?		# if no argument is given on command line, default to today's date
-	ARGV.each { |a|		# for each command-line arg, i.e. date...
+	ARGV.each do |a|		# for each command-line arg, i.e. date...
 		todaysnoticedir = __dir__ + "/notices/#{a}"
 		FileUtils.mkdir_p(todaysnoticedir)
 		checkdate = Date.parse(a)
 		puts checkdate
 		count = 0
-		filelist.each { |file|
+		filelist.each do |file|
 			if ftp.mtime(file).to_date == checkdate then	# compare file's mtime to supplied check date
-				with_retries(:max_tries => 3, :rescue => [Net::FTPReplyError, Net::ReadTimeout]) do |attnum|
-					puts "getting #{file} (attempt ##{attnum})..."
-					localpath = __dir__ + "/notices/#{a}/#{file}"
-					ftp.gettextfile(file, localpath)
+				begin
+					with_retries(:max_tries => 3, :rescue => [Net::FTPReplyError, Net::ReadTimeout]) do |attnum|
+						puts "getting #{file} (attempt ##{attnum})..."
+						localpath = __dir__ + "/notices/#{a}/#{file}"
+						ftp.gettextfile(file, localpath)
+					end
+				rescue Net::FTPReplyError
+					puts "#{file} failed"
+				rescue Net::ReadTimeout
+					puts "#{file} failed"
 				end
 				count += 1
 			end
-		}
+		end
 		puts "total count: #{count}"
-	}
+	end
 
 	ftp.close
 end
