a1a49f366d74c735584b25c10ee6831b1b46ccaa
dojobo <dominicbordelon@gmail.com>
2015-07-21 15:17:36 -0500

    add comments

--- getemailnotices.rb
+++ getemailnotices.rb
@@ -14,10 +14,12 @@ Net::FTP.open(server) do |ftp|
 	ftp.login(user, pass)
 	print "connected to #{server} - "
 	puts ftp.pwd
+	# modify filelist pattern as necessary:
 	#filelist = %w(em10* em11* em12* em13* em14* em15* em16* em17* em18* em19* em20* em21* em22* em23*).each_with_object([]) { |range, filelist|
 	filelist = %w(em20* em21* em22* em23*).each_with_object([]) do |range, filelist|
   	begin
     	filelist.concat ftp.nlst(range)
+    # handle exceptions resulting from nlst requests with zero results:
   	rescue Net::FTPPermError
   	end
 	end
@@ -31,11 +33,13 @@ Net::FTP.open(server) do |ftp|
 		filelist.each do |file|
 			if ftp.mtime(file).to_date == checkdate then	# compare file's mtime to supplied check date
 				begin
+					# try each file up to 3 times, rescuing for timeouts
 					with_retries(:max_tries => 3, :rescue => [Net::FTPReplyError, Net::ReadTimeout]) do |attnum|
 						puts "getting #{file} (attempt ##{attnum})..."
 						localpath = __dir__ + "/notices/#{a}/#{file}"
 						ftp.gettextfile(file, localpath)
 					end
+				# in case all three attempts fail, rescue and move on to next file:
 				rescue Net::FTPReplyError
 					puts "#{file} failed"
 				rescue Net::ReadTimeout
