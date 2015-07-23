f2ef80c79be9cb835243cd8cae53453bd29bbd56
dojobo <dominicbordelon@gmail.com>
2015-07-23 14:20:32 -0500

    add rescue next; remove retries

--- getemailnotices.rb
+++ getemailnotices.rb
@@ -21,6 +21,7 @@ Net::FTP.open(server) do |ftp|
       filelist.concat ftp.nlst(range)
     # handle exceptions resulting from nlst requests with zero results:
     rescue Net::FTPPermError
+      next
     end
   end
   ARGV << Date.today.to_s if ARGV.empty?    # if no argument is given on command line, default to today's date
@@ -33,17 +34,16 @@ Net::FTP.open(server) do |ftp|
     filelist.each do |file|
       if ftp.mtime(file).to_date == checkdate then  # compare file's mtime to supplied check date
         begin
-          # try each file up to 3 times, rescuing for timeouts
-          with_retries(:max_tries => 3, :rescue => [Net::FTPReplyError, Net::ReadTimeout]) do |attnum|
-            puts "getting #{file} (attempt ##{attnum})..."
-            localpath = __dir__ + "/notices/#{a}/#{file}"
-            ftp.gettextfile(file, localpath)
-          end
-        # in case all three attempts fail, rescue and move on to next file:
+          puts "getting #{file}..."
+          localpath = __dir__ + "/notices/#{a}/#{file}"
+          ftp.gettextfile(file, localpath)
+        # in case of failure, rescue and move on to next file:
         rescue Net::FTPReplyError
           puts "#{file} failed"
+          next
         rescue Net::ReadTimeout
           puts "#{file} failed"
+          next
         end
         count += 1
       end
